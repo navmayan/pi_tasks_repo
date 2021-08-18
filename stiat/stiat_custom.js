baec70cf966c9824e3b217d0f00d80d9118fa0b7
navmayan <49531003+navmayan@users.noreply.github.com>
2021-08-18 11:28:50 +0300

    udpate custom script

--- stiat/stiat_custom.js
+++ stiat/stiat_custom.js
@@ -60,51 +60,51 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 			css : {color:'#31b404','font-size':'3em'}
 			},
 
-      trialsByBlock :
-      			[//Each object in this array defines a block
-      				{
-      					instHTML : '', //Empty means we will create the inst from the instTemplate variable further below.
-      					block : 1, //The block variable is not used later, but could help the user.
-      					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
-      					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
-      					singleAttTrials : 2,//10, //Number of trials of the attribute that does not share key with the category (in a mini block).
-      					sharedAttTrials : 2, // 10, //Number of trials of the attribute that shares key with the category (in a mini block).
-      					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
-      					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
-      				},
-      				{
-      					instHTML : '',
-      					block : 2,
-      					miniBlocks : 2,
-      					singleAttTrials : 2, //10,
-      					sharedAttTrials : 2, //7,
-      					categoryTrials : 2 //7
-      				},
-      				{
-      					instHTML : '',
-      					block : 3,
-      					miniBlocks : 2,
-      					singleAttTrials : 2, //10,
-      					sharedAttTrials : 2, //7,
-      					categoryTrials : 2 //7
-      				},
-      				{
-      					instHTML : '',
-      					block : 4,
-      					miniBlocks : 2,
-      					singleAttTrials : 2, //10,
-      					sharedAttTrials : 2, //7,
-      					categoryTrials : 2 //7
-      				},
-      				{
-      					instHTML : '',
-      					block : 5,
-      					miniBlocks : 2,
-      					singleAttTrials : 2, //10,
-      					sharedAttTrials : 2, //7,
-      					categoryTrials : 2 //7
-      				}
-      			],
+trialsByBlock :
+			[//Each object in this array defines a block
+				{
+					instHTML : '', //Empty means we will create the inst from the instTemplate variable further below.
+					block : 1, //The block variable is not used later, but could help the user.
+					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
+					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
+					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
+					sharedAttTrials : 10, //Number of trials of the attribute that shares key with the category (in a mini block).
+					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
+					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
+				},
+				{
+					instHTML : '',
+					block : 2,
+					miniBlocks : 1,
+					singleAttTrials : 1,
+					sharedAttTrials : 1,
+					categoryTrials : 1
+				},
+				{
+					instHTML : '',
+					block : 3,
+					miniBlocks : 1,
+					singleAttTrials : 1,
+					sharedAttTrials : 1,
+					categoryTrials : 1
+				},
+				{
+					instHTML : '',
+					block : 4,
+					miniBlocks : 1,
+					singleAttTrials : 1,
+					sharedAttTrials : 1,
+					categoryTrials : 1
+				},
+				{
+					instHTML : '',
+					block : 5,
+					miniBlocks : 1,
+					singleAttTrials : 1,
+					sharedAttTrials : 1,
+					categoryTrials : 1
+				}
+			],
 
   base_url : {//Where are your images at?
     image : 'https://baranan.github.io/minno-tasks/images/'
